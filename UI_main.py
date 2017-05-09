c464796fd9978fa44df263967a6964d623a7a647
zui <626255007@qq.com>
2017-05-09 18:57:27 +0800

    .

---
+++ UI_main.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt4 import QtGui, uic
+
+qtCreatorFile_main = "uis/main.ui"
+qtCreatorFile_l1 = "uis/l1.ui"
+qtCreatorFile_l2 = "uis/l2.ui"
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile_main)
+Ui_l1Window, Qtl1Class = uic.loadUiType(qtCreatorFile_l1)
+Ui_l2Window, Qtl2Class = uic.loadUiType(qtCreatorFile_l2)
+
+
+class MyApp(QtGui.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.l1 = l1()
+        self.trainButton.clicked.connect(self.t)
+        self.runButton.clicked.connect(self.r)
+
+    def t(self):
+        print("Train...")
+        self.l1.show()
+
+    def r(self):
+        print("Run...")
+
+
+class l1(QtGui.QMainWindow, Ui_l1Window):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self)
+        Ui_l1Window.__init__(self)
+        self.setupUi(self)
+        self.loadButton.clicked.connect(self.load)
+        self.pushButton.clicked.connect(self.print_)
+        self.l2 = l2()
+
+    def load(self):
+        print("Load...")
+
+    def print_(self):
+        self.l2.show()
+        print("Print...")
+
+
+class l2(QtGui.QMainWindow, Ui_l2Window):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self)
+        Ui_l2Window.__init__(self)
+        self.setupUi(self)
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
